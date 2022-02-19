a95a4a87d85f159aa3aacc0b4878101dc3e81411
Javi Fontan <jfontan@gmail.com>
2022-02-19 14:19:51 +0100

    Show permissions, links and owneship

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::fs;
 use std::io;
 use std::io::prelude::*;
+use std::os::unix::fs::MetadataExt;
 use tempfile::tempdir;
 
 fn create_test_files() -> io::Result<String> {
@@ -27,28 +28,59 @@ fn delete_test_files(path: String) -> io::Result<()> {
     Ok(())
 }
 
-const mode_string: &[u8] = b"rwxrwxrwx";
+const MODE_STRING: &[u8] = b"rwxrwxrwx";
 
 fn format_mode(mode: u32) -> String {
     let mut str = String::from("");
     let mut mask = 0b100_000_000;
 
-    for i in 8..=0 {
+    println!("{}", mode);
+
+    for i in 0..=8 {
         let r = mode & mask;
         if r > 0 {
-            str.push(mode_string[i] as char);
+            str.push(MODE_STRING[i] as char);
         } else {
             str.push('-');
         }
-
+        mask = mask >> 1;
     }
     str
 }
 
-fn format_file(path: String) -> io::Result<String> {
-    let metadata: fs::MetadataEx = fs::symlink_metadata(path)?;
-    mode = format_mode(metadata.
-    Ok("".to_string())
+fn format_user(uid: u32) -> String {
+    match users::get_user_by_uid(uid) {
+        Some(user) => user.name().to_str().unwrap().to_string(),
+        None => uid.to_string(),
+    }
+}
+
+fn format_group(gid: u32) -> String {
+    match users::get_group_by_gid(gid) {
+        Some(group) => group.name().to_str().unwrap().to_string(),
+        None => gid.to_string(),
+    }
+}
+
+fn format_file(entry: &fs::DirEntry) -> io::Result<String> {
+    let path = entry.path();
+    let metadata = fs::symlink_metadata(path)?;
+
+    let directory = if metadata.is_dir() { "d" } else { "-" };
+    let mode = format_mode(metadata.mode());
+
+    let links = metadata.nlink();
+
+    let line = format!(
+        "{}{} {} {} {} {}",
+        directory,
+        mode,
+        links,
+        format_user(metadata.uid()),
+        format_group(metadata.gid()),
+        entry.file_name().to_str().unwrap(),
+    );
+    Ok(line)
 }
 
 fn main() -> std::io::Result<()> {
@@ -57,8 +89,8 @@ fn main() -> std::io::Result<()> {
     let dir = fs::read_dir(&path)?;
     for entry in dir {
         let file = entry?;
-        println!("{:?} {:?}", file.file_name(), file.file_type());
-        format_file(file.path().to_str().unwrap().to_owned())?;
+        let line = format_file(&file)?;
+        println!("{}", line);
     }
 
     delete_test_files(path)
